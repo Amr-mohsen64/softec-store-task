96fd0e57f3a58e0b0d83a81977fde118e10cb7fe
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-10-11 05:30:01 +0200

    list dynamic products array at prouduct page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductCardComponent } from './components/products/product-card/product-card.component';
 import { ProductsComponent } from './components/products/products/products.component';
-import { FooterComponent } from './components/footer/footer.component';
-
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, ProductCardComponent, ProductsComponent, FooterComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProductCardComponent,
+    ProductsComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, NgbModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/products/products/products.component.ts
+++ src/app/components/products/products/products.component.ts
@@ -1,15 +1,19 @@
+import { ProductService } from './../../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((products: Product[]) => {
+      this.products = products;
+    });
   }
-
 }
