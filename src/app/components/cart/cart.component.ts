d0535a6b64cfdde4f861a3d82851f3f90edb2193
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-10-15 17:30:41 +0200

    Added select options (users , payment type) at cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,10 @@
+import { User } from 'src/app/models/user.model';
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { Product } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/models/order.model';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -10,10 +13,14 @@ import { CartService } from 'src/app/services/cart.service';
 export class CartComponent implements OnInit {
   cartProducts: Product[] = [];
   totalPrice: number = 0;
+  users: User[] = [];
+  selectedUserId!: string;
+  paymentType: string = 'cash';
 
   constructor(
     public activeOffcanvas: NgbActiveOffcanvas,
-    private cartService: CartService
+    private cartService: CartService,
+    private orderService: OrderService
   ) {}
 
   ngOnInit(): void {
@@ -24,6 +31,34 @@ export class CartComponent implements OnInit {
     this.cartService.cartTotalPrice.subscribe(
       (price) => (this.totalPrice = price)
     );
+    this.orderService.getUsers();
+    this.orderService.users.subscribe((users) => {
+      this.users = users;
+      this.selectedUserId = this.users[0]?.Id;
+    });
     console.log(this.totalPrice);
+  }
+
+  onUsersChange(userId: string) {
+    this.selectedUserId = userId;
+  }
+
+  onAddOrder() {
+    const newOrder: Order = {
+      Products: this.cartProducts,
+      OrderId: Math.ceil(Math.random() * (10000 - 1000) + 1000),
+      UserId: this.selectedUserId,
+      OrderDate: new Date().toString(),
+      PaymentType: this.paymentType,
+    };
+
+    console.log(newOrder);
+    this.emptyCart();
+  }
+
+  emptyCart() {
+    this.cartService.productsCount.next(0);
+    this.cartProducts = [];
+    this.cartService.cartProducts = [];
   }
 }
